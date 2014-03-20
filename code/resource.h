9b62c2239a17ae00911e824c00ff4a81d262051d
Andrew Bibb <ajbibb@myfairpoint.net>
2014-03-19 20:24:41 -0400

    fixed up release date

--- code/resource.h
+++ code/resource.h
@@ -35,7 +35,7 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 // Program Info 
 #define VERSION "14.03.13"
-#define RELEASE_DATE "5 January 2014"
+#define RELEASE_DATE "13 March 2014"
 #define COPYRIGHT_DATE "2013-2014"
 
 // Program Values
