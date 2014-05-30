68784adcdcb509f445a20fd6b9b065137a45c47f
Andrew Bibb <ajbibb@myfairpoint.net>
2014-05-30 19:02:58 -0400

    changed text relating to tooltips and system tray popups

--- code/resource.h
+++ code/resource.h
@@ -34,7 +34,7 @@ DEALINGS IN THE SOFTWARE.
 
 ///////////////////////////////// Program Values ///////////////////////
 // Program Info 
-#define VERSION "14.05.29-1"
+#define VERSION "14.05.30-1"
 #define RELEASE_DATE "10 May 2014"
 #define COPYRIGHT_DATE "2013-2014"
 
