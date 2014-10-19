d17ad84a411d8036798108b7fedca0db9c800459
Andrew Bibb <ajbibb@myfairpoint.net>
2014-10-19 15:26:33 -0400

    removed code dir

--- code/cmstapp/counter/cpp2xml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#	script to convert the header file to cpp
-#
-#	-M	all public slots
-#	-P	all properties
-# -S  all signals
-qdbuscpp2xml -M -P counter.h -o org.monkey_business_enterprises.counter.xml
