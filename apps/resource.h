c3d38eeb732f142b5686862c0de6d2e7b0246674
Andrew Bibb <ajbibb@myfairpoint.net>
2015-10-02 20:55:44 -0400

    txt and changelog files for 2015.10.03 release

--- apps/resource.h
+++ apps/resource.h
@@ -35,8 +35,8 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 //
 // Program Info (may be visible, but don't mark for tranalation) 
-#define VERSION "2015.09.21-2"
-#define RELEASE_DATE "1 March 2015"
+#define VERSION "2015.10.03-1"
+#define RELEASE_DATE "3 October 2015"
 #define COPYRIGHT_DATE "2013-2015"
 
 // Program Values:
