eaf9442d03c3f393955b47b053d1dd63b458f5c0
Andrew Bibb <ajbibb@myfairpoint.net>
2015-12-02 18:52:30 -0500

    commit before release of 2015.12.03

--- apps/resource.h
+++ apps/resource.h
@@ -35,8 +35,8 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 //
 // Program Info (may be visible, but don't mark for tranalation) 
-#define VERSION "2015.12.01-1"
-#define RELEASE_DATE "3 October 2015"
+#define VERSION "2015.12.03-1"
+#define RELEASE_DATE "3 December 2015"
 #define COPYRIGHT_DATE "2013-2015"
 
 // Program Values:
