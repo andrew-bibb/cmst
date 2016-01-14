20cb65cd3b4ca72a299b937a00ae185ed05813c8
Andrew Bibb <ajbibb@myfairpoint.net>
2016-01-14 18:08:59 -0500

    removed vpn_agent

--- apps/cmstapp/code/vpn_agent/cpp2xml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-#	script to convert the header file to cpp
-#
-#	-M	all public slots
-#	-P	all properties
-qdbuscpp2xml -M -P vpnagent.h -o org.monkey_business_enterprises.vpnagent.xml
