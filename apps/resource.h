d63470ea6a194650bb77b09f19ddffa4946d8177
Andrew Bibb <ajbibb@myfairpoint.net>
2016-03-05 13:47:17 -0500

    validating dialog moved to shared

--- apps/resource.h
+++ apps/resource.h
@@ -35,7 +35,7 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 //
 // Program Info (may be visible, but don't mark for tranalation) 
-#define VERSION "2016.03.03-1"
+#define VERSION "2016.03.05-1"
 
 #define RELEASE_DATE "26 January 2016"
 #define COPYRIGHT_DATE "2013-2016"
