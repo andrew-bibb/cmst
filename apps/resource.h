9f7efcbb51e4399cff3922d8ca5f11cda2aad800
Andrew Bibb <ajbibb@outlook.com>
2021-04-30 18:35:58 -0400

    change log and release info for PR 233

--- apps/resource.h
+++ apps/resource.h
@@ -35,7 +35,7 @@ DEALINGS IN THE SOFTWARE.
 ///////////////////////////////// Program Values ///////////////////////
 //
 // Program Info (may be visible, but don't mark for tranalation)
-#define VERSION "2021.03.28-1"
+#define VERSION "2021.04.30-1"
 
 #define RELEASE_DATE "1 November 2020"
 #define COPYRIGHT_DATE "2013-2021"
